142c96cdf3eaaec8947a99f537c234d9f455f89d
data-miner00 <data-miner00@users.noreply.github.com>
2021-10-08 20:03:42 +0800

    feat: Added 144A

---
+++ attempts/144A/solution144A.cpp
@@ -0,0 +1,23 @@
+#include "solution144A.h"
+#include <iostream>
+
+void setup() {
+    int n;
+    int min = 100, max = 2;
+    int minindex, maxindex;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        std::cin >> temp;
+        if (temp > max) {
+            max = temp;
+            maxindex = i;
+            continue;
+        }
+        if (temp <= min) {
+            min = temp;
+            minindex = i;
+        }
+    }
+    std::cout << maxindex + ((n - 1) - minindex);
+}
---
+++ attempts/144A/solution144A.h
@@ -0,0 +1,6 @@
+#ifndef SOLUTION144A_H
+#define SOLUTION144A_H
+
+void setup();
+
+#endif

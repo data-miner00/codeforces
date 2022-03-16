617bc0e80d129643f2a2933c97c03a3959397c30
data-miner00 <data-miner00@users.noreply.github.com>
2022-03-16 19:51:22 +0800

    docs: William Lin coding template

---
+++ templates/workspace.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+    // Logic here
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
